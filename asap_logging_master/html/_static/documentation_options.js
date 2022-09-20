942e6db034ffdc2ba332b78d87ea8e756c56a963
abdes <abdes@users.noreply.github.com>
2022-09-20 18:38:41 +0000

    deploy: df96500668ac6b05d0930e97d61e9159bfb70995

--- asap_logging_master/html/_static/documentation_options.js
+++ asap_logging_master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2 (59d867efdb45)',
+    VERSION: '1.0.2 (df96500668ac)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
